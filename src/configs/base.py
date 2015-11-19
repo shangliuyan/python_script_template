c54abdd7609d0f006622e0278e715f1c3e7bb7e6
shangliuyan <shangliuyan@gmail.com>
2015-11-19 10:56:03 +0800

    change log handler

--- src/configs/base.py
+++ src/configs/base.py
@@ -1,13 +1,14 @@
 
 import os
 import logging
+from logging.handlers import WatchedFileHandler
 
 CURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
-PROJECT_ROOT = os.path.dirname(os.path.dirname(CURRENT_FOLDER))
-LOG_ROOT = os.path.join(PROJECT_ROOT, "log")
+PROJECT_PATH = os.path.dirname(os.path.dirname(CURRENT_FOLDER))
+LOG_PATH = os.path.join(PROJECT_PATH, "log")
 
-if not os.path.exists(LOG_ROOT):
-    os.makedirs(LOG_ROOT)
+if not os.path.exists(LOG_PATH):
+    os.makedirs(LOG_PATH)
 
 LOG_FORMAT = '%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s - %(message)s'
 formatter = logging.Formatter(LOG_FORMAT)
@@ -16,16 +17,16 @@ debug_log_handler = logging.StreamHandler()
 debug_log_handler.setLevel(logging.DEBUG)
 debug_log_handler.setFormatter(formatter)
 
-info_log_handler = logging.FileHandler(os.path.join(LOG_ROOT, "info.log"))
+info_log_handler = WatchedFileHandler(os.path.join(LOG_PATH, "info.log"))
 info_log_handler.setLevel(logging.INFO)
 info_log_handler.setFormatter(formatter)
 
-error_log_handler = logging.FileHandler(os.path.join(LOG_ROOT, "error.log"))
+error_log_handler = WatchedFileHandler(os.path.join(LOG_PATH, "error.log"))
 error_log_handler.setLevel(logging.ERROR)
 error_log_handler.setFormatter(formatter)
 
 LOGGER = logging.getLogger("project_name")
-LOGGER.setLevel(logging.Debug)
+LOGGER.setLevel(logging.DEBUG)
 LOGGER.addHandler(debug_log_handler)
 LOGGER.addHandler(info_log_handler)
 LOGGER.addHandler(error_log_handler)
